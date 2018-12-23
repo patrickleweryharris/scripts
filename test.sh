06020e32af2de0ffc85851b1d3f9317a990b865f
Patrick Harris <patricklewery.harris@mail.utoronto.ca>
2018-12-23 18:23:03 -0500

    Add python and bash syntx checking to test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Find instances of home dir in literal form and flag them
 # Need to seperate HOMEDIR out so this script doesn't get flagged
 HOMEDIR='/Users'
@@ -11,6 +10,25 @@ then
     CODE=0
 else
     echo $BADHOME >&2
+fi
+
+# Do basic bash syntax checking on all bash scripts in dir
+for SCRIPT in `grep -lRe "bin/bash" *`
+do
+    bash -n $SCRIPT
+    CHECK=$?
+    if [[ $CHECK -ne 0 ]]
+    then
+        CODE=1
+    fi
+done
+
+# Do the same for python
+flake8 .
+CHECK=$?
+if [[ $CHECK -ne 0 ]]
+then
+    CODE=1
 fi
 
 exit $CODE
