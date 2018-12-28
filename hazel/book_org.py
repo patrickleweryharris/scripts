8944cb3771f8b29a259d195a55eefb1c4f9453b2
Patrick Harris <patricklewery.harris@mail.utoronto.ca>
2018-12-27 19:53:12 -0500

    Add error checking to hazel script

--- hazel/book_org.py
+++ hazel/book_org.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 import os
-import sys
+import re
 import shutil
+import argparse
 """
 Script used to organize my epub books.
 Books are expected to be placed in $HOME/Dropbox/books
@@ -9,14 +10,26 @@ Books are expected to be placed in $HOME/Dropbox/books
 BOOK_PATH = os.getenv("HOME") + '/Dropbox/books'
 
 
-if __name__ == '__main__':
-    # Book is expected to be in the following format:
-    # last, first - <other info>.epub
-    BOOK = sys.argv[1]
-    SIMPLE = BOOK.split("/")[-1]
-    last = BOOK.split(",")[0]
+def create_parser():
+    """ Create argparse object for this CLI """
+    parser = argparse.ArgumentParser(
+        description="Process ebooks in this format: last, first - <...>.epub")
+    parser.add_argument("book_name", metavar="BOOK", type=check_name_matches,
+                        help="Epub file to process")
+
+    return parser
+
+
+def run(book):
+    """
+    Move book to dir for the author.
+
+    Create dir for author if it does not exist
+    """
+    simple = book.split("/")[-1]
+    last = book.split(",")[0]
     last = last.split("/")[-1].strip()
-    first = BOOK.split(",")[1]
+    first = book.split(",")[1]
     first = first.split("-")[0].strip()
 
     full = first + " " + last
@@ -24,4 +37,19 @@ if __name__ == '__main__':
     # If a directory for that author does not exist, create it
     if not os.path.exists(full):
         os.mkdir(full)
-    shutil.move(BOOK, full + "/" + SIMPLE)
+    shutil.move(book, full + "/" + simple)
+
+
+def check_name_matches(book):
+    cut = book.split("/")[-1]
+    book_syntax = r"[A-Za-z]+, [A-Za-z\.]+[ A-Za-z\.]* - .*(\.epub)"
+    if not re.match(book_syntax, cut):
+        raise argparse.ArgumentTypeError(
+            "{} is an invalid file name".format(book))
+    return book
+
+
+if __name__ == '__main__':
+    parser = create_parser()
+    args = parser.parse_args()
+    run(args.book_name)
