b1ea8cabd4f408f8cc698176927d3c9765ec4be7
Patrick Harris <patricklewery.harris@mail.utoronto.ca>
2018-12-28 21:46:50 -0500

    Add support for book names without a comma

--- hazel/book_org.py
+++ hazel/book_org.py
@@ -20,29 +20,45 @@ def create_parser():
     return parser
 
 
-def run(book):
+def first_last(book):
+    """
+    Match Format: last, first - <...>.epub
+    """
+    filename = book.split("/")[-1]
+    last = filename.split(",")[0].strip()
+    first = book.split(",")[1]
+    first = first.split("-")[0].strip()
+
+    author = first + " " + last
+
+    file_book(book, author, filename)
+
+
+def no_comma(book):
+    """
+    Match Format: <name> - <...>.epub
+    """
+    filename = book.split("/")[-1]
+    author = filename.split("-")[0].strip()
+    file_book(book, author, filename)
+
+
+def file_book(book, author, filename):
     """
     Move book to dir for the author.
 
     Create dir for author if it does not exist
     """
-    simple = book.split("/")[-1]
-    last = book.split(",")[0]
-    last = last.split("/")[-1].strip()
-    first = book.split(",")[1]
-    first = first.split("-")[0].strip()
-
-    full = first + " " + last
-    full = BOOK_PATH + "/" + full
+    author = BOOK_PATH + "/" + author
     # If a directory for that author does not exist, create it
-    if not os.path.exists(full):
-        os.mkdir(full)
-    shutil.move(book, full + "/" + simple)
+    if not os.path.exists(author):
+        os.mkdir(author)
+    shutil.move(book, author + "/" + filename)
 
 
 def check_name_matches(book):
     cut = book.split("/")[-1]
-    book_syntax = r"[A-Za-z ]+, [A-Za-z\.]+[ A-Za-z\.]* - .*(\.epub)"
+    book_syntax = r"[ A-Za-z\.,]* - .*(\.epub)"
     if not re.match(book_syntax, cut):
         raise argparse.ArgumentTypeError(
             "{} is an invalid file name".format(book))
@@ -52,4 +68,10 @@ def check_name_matches(book):
 if __name__ == '__main__':
     parser = create_parser()
     args = parser.parse_args()
-    run(args.book_name)
+    book = args.book_name
+    # last, first - <...>.epub
+    if ',' in book and book.find(',') < book.find('-'):
+        first_last(book)
+    # <name> - <...>.epub
+    else:
+        no_comma(book)
