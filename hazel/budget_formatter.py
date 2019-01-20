2eb5a4993d08d5ea93f18663b917a5177817e67c
Patrick Harris <patricklewery.harris@mail.utoronto.ca>
2019-01-20 00:46:20 -0500

    Add budget formatter for Hazel

---
+++ hazel/budget_formatter.py
@@ -0,0 +1,29 @@
+#!/usr/local/bin/python3
+import os
+import argparse
+import pandas as pd
+
+METRICS_PATH = os.getenv("HOME") + '/Dropbox/metrics/to_process/parsed.csv'
+
+
+def create_parser():
+    """ Create argparse object for this CLI """
+    parser = argparse.ArgumentParser(
+        description="Format downloaded CC reports to match with spreadsheet")
+    parser.add_argument("filename", metavar="FILE",
+                        help="CSV file to process")
+
+    return parser
+
+
+def process_file(filename):
+    """ Turn the file into the correct format """
+    f = pd.read_csv(filename, usecols=['Merchant Name', 'Date', 'Amount'])
+    f = f[['Date', 'Merchant Name', 'Amount']]
+    f.to_csv(METRICS_PATH, index=False)
+
+
+if __name__ == '__main__':
+    parser = create_parser()
+    args = parser.parse_args()
+    process_file(args.filename)
